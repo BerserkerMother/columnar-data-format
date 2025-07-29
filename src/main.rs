ce068e0fd85a14ceb8052dcfa51b9b22289595ad
BerserkerMother <sadegh.b218@gmail.com>
2025-07-29 21:58:22 +0330

    early design of number column

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ mod storage;
 mod text;
 mod util;
 
-fn main() {}
+fn main() {
+    let col = database::NumberColumn::test_new();
+    println!("{:?}", col);
+    dbg!(size_of::<Option<u64>>());
+    dbg!(align_of::<usize>());
+}
